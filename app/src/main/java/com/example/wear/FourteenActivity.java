bb7ea4d57e41ccb6baae6cd5b40b26412bb3f966
liyinchi_gitlab <liyinchi@qq.com>
2023-10-06 01:54:46 +0800

    相机和相册

--- app/src/main/java/com/example/wear/FourteenActivity.java
+++ app/src/main/java/com/example/wear/FourteenActivity.java
@@ -3,6 +3,7 @@ package com.example.wear;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -14,7 +15,8 @@ import com.example.wear.databinding.ActivityFourteenBinding;
 
 public class FourteenActivity extends Activity {
 
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;//
+    private static final int REQUEST_IMAGE_PICK = 2;
     private TextView mTextView;
     private ActivityFourteenBinding binding;
 
@@ -26,6 +28,7 @@ public class FourteenActivity extends Activity {
         setContentView(binding.getRoot());
 
 //        mTextView = binding.text;
+        // 打开相机
         Button btnOpenCamera = findViewById(R.id.btnOpenCamera);
         btnOpenCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,12 +45,29 @@ public class FourteenActivity extends Activity {
                 }
             }
         });
+        // 打开相册
+        Button btnOpenGallery = findViewById(R.id.btnOpenGallery);
+        btnOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建一个隐式的Intent，指定ACTION_PICK动作和图片类型
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                // 检查设备是否有相册应用可用
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    // 启动相册应用，并等待选择结果
+                    startActivityForResult(intent, REQUEST_IMAGE_PICK);
+                } else {
+                    Toast.makeText(FourteenActivity.this, "无法打开相册", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        // 相机
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             // 从Intent中获取拍摄结果照片
             Bundle extras = data.getExtras();
@@ -56,5 +76,15 @@ public class FourteenActivity extends Activity {
             // 在这里处理拍摄结果照片
             // 您可以将照片显示在ImageView中，保存到本地等
         }
+        // 相册
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
+            // 从Intent中获取选中的照片的URI
+            Uri selectedImageUri = data.getData();
+
+            // 在这里处理选中的照片
+            // 您可以将照片显示在ImageView中，保存到本地等
+        }
     }
+
+
 }
